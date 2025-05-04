81c117c78338a63bcb02fa6dccd421a22491147f
Avraham <souzaavraham@proton.me>
2025-05-04 14:56:25 -0300

    feat(user): add and list pages

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,27 +1,61 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.controller.dto.CriarUsuarioDto;
+import com.example.demo.factory.UsuarioAdministradorFactory;
+import com.example.demo.factory.UsuarioComumFactory;
+import com.example.demo.factory.UsuarioFactory;
+import com.example.demo.model.BuilderUsuario;
+import com.example.demo.model.Usuario;
+import com.example.demo.service.UsuarioService;
+
 @Controller
-@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
-  @GetMapping
-  public String teste() {
-    return "Teste";
+  private final UsuarioService usuarioService;
+
+  public UsuarioController(UsuarioService usuarioService) {
+    this.usuarioService = usuarioService;
   }
-    // @GetMapping("/cadastro")
-    // public String mostrarFormulario(Model model) {
-    //     model.addAttribute("usuario", new Usuario());
-    //     return "cadastro"; // Thymeleaf procura templates/cadastro.html
-    // }
-    //
-    // @PostMapping("/usuarios")
-    // public String cadastrarUsuario(@ModelAttribute Usuario usuario) {
-    //     RepositorioUsuarios.getInstance().adicionarUsuario(usuario);
-    //     return "redirect:/usuarios";
-    // }
+
+  @GetMapping("/novo")
+  public String cadastro() {
+    return "cadastro";
+  }
+
+  @GetMapping
+  public String listar(Model model) {
+    model.addAttribute("usuarios", usuarioService.listarUsuarios());
+    return "listar";
+  }
+
+  @PostMapping
+  public String cadastrarUsuario(@ModelAttribute CriarUsuarioDto dto) {
+    try {
+
+      BuilderUsuario builder = new BuilderUsuario(dto.nome(), dto.email(), dto.cpf());
+      builder.setTelefone(dto.telefone());
+      builder.setEndereco(dto.endereco());
+
+      System.out.println(dto);
+      UsuarioFactory factory = dto.tipo().equals("admin")
+        ? new UsuarioAdministradorFactory()
+        : new UsuarioComumFactory();
+
+      Usuario usuario = factory.criarUsuario(builder);
+
+      usuarioService.cadastrarUsuario(usuario);
+      return "redirect:/usuarios";
+
+    } catch (Exception err) {
+      System.out.println(err);
+      return "redirect:/usuarios";
+    }
+  }
     //
     // @GetMapping("/usuarios")
     // public String listarUsuarios(Model model) {
---
+++ src/main/java/com/example/demo/controller/dto/CriarUsuarioDto.java
@@ -0,0 +1,6 @@
+package com.example.demo.controller.dto;
+
+/**
+ * CriarUsuarioDto
+ */
+public record CriarUsuarioDto(String nome, String email, String cpf, String telefone, String endereco, String tipo) {}
